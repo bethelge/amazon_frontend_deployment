f99c73a023cb51ee3178b83d60992f43cd1485e5
Bethel Gelan <bethelgelan16@gmail.com>
2024-12-09 20:55:00 +0300

    vite config

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  css: {
+    modules: {
+      scopeBehaviour: 'local', // This ensures the class names are locally scoped
+    },
+  },
+});
